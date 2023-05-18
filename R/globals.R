5b53fa8e7dda27de4b5560fe64ec4b1ad819371c
jackbibby1 <59352321+jackbibby1@users.noreply.github.com>
2023-05-18 09:27:02 -0400

    add globals

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c("Chr", "Start", "End", "Strand", "Length", "Geneid", "pc1", "pc2", "n", "pc", "var", "group", "gene"))
+utils::globalVariables(c("Chr", "Start", "End", "Strand", "Length", "Geneid", "pc1", "pc2", "n", "pc", "var", "group", "gene", "%notin%"))
